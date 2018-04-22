2d103b8fa5b9a0f6442b59d36223582587cd1ec9
Mark <1007004@student.sae.edu.au>
2018-04-22 21:06:18 +1000

    testing out obstacle mechanics with trigger points

---
+++ Assets/Greybox_Testing/Obstacle_Triggering.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Triggering : MonoBehaviour {
+
+    public GameObject obstacle;
+    public GameObject player;
+    public Obstacle_Prototype CP;
+    public GameObject worldLayout;
+
+    public Level_Movement LM;
+    public Obstacle_Prototype OP;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        CP.enabled = true;
+        player.gameObject.SetActive(false);
+        worldLayout.gameObject.SetActive(false);
+        LM.enabled = false;
+        OP.enabled = true;
+        obstacle.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/Obstacle_Prototype/Obstacle_Dot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle_Dot : MonoBehaviour
+{
+    public GameObject ray_position;
+
+    private LineRenderer lr;
+
+    private Obstacle_Prototype op;
+    private float distance;
+
+    private bool locked;
+
+	// Use this for initialization
+	void Start ()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, transform.position);
+
+        op = GameObject.Find("Main Camera").GetComponent<Obstacle_Prototype>();
+    }
+
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distance = Vector3.Distance(transform.position, op.pointer_world_position);
+
+        if(distance < 0.2f && op.dragging && !locked && gameObject.tag != "obstacle_immoveable")
+        {
+            locked = true;
+            op.Change_Dot (gameObject, lr, locked);
+        }
+	}
+}
