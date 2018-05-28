1864d391089f24909f1862a0f14056a1c556678d
Christopher Wrigley <cwrigley@sae.au>
2018-05-28 17:53:34 +1000

    added base scene

---
+++ Assets/Greybox_Testing/Multiple_Scene_Greybox/MSG_Transitioner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MSG_Transitioner : MonoBehaviour
+{
+    public string level_name;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SceneManager.LoadSceneAsync(level_name, LoadSceneMode.Additive);
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SceneManager.UnloadSceneAsync(level_name);
+        }
+    }
+}
