039590a3528acab7e8580ca3015f65fe332d83ab
Mark <1007004@student.sae.edu.au>
2018-06-24 12:45:22 +1000

    Welcome Trigger
    Player enters trigger zone plays Welcome and Begin audio

---
+++ Assets/Greybox_Testing/Tutorial_Scene_Greybox/Scripts/Tutorial_Triggers.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_Triggers : MonoBehaviour
+{
+    [Header("Universal Scripts")]
+    public MSG_Level_Movement msgLevelMovement;
+
+    [Header("Gameobject Triggers")]
+    public GameObject welcomeTrigger;
+
+    [Header("Audio")]
+    public AudioSource audioMain;
+    public AudioClip welcomeVoice;
+    public AudioClip beginVoice;
+
+    void Start()
+    {
+        audioMain = GetComponent<AudioSource>();
+        //Find The GameObject's AudioSource
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            WelcomeDialogue();
+            //If The Trigger Detects The Player Tag Instatiate The WelcomeDialogue
+        }
+    }
+
+    void WelcomeDialogue()
+    {
+        StartCoroutine(playWelcomeDialogue());
+
+        msgLevelMovement.enabled = false;
+        //Disable Level Movement
+    }
+
+    IEnumerator playWelcomeDialogue()
+    {
+        audioMain.clip = welcomeVoice;
+        audioMain.Play();
+        yield return new WaitForSeconds(audioMain.clip.length);
+        audioMain.clip = beginVoice;
+        audioMain.Play();
+
+        Invoke("WelcomeDialogueEnd", audioMain.clip.length);
+        //Once DialogueVoice Has Reached The End Of The Length (Seconds) Invoke WelcomeDialogueEnd
+    }
+
+    void WelcomeDialogueEnd()
+    {
+        welcomeTrigger.SetActive(false);
+        msgLevelMovement.enabled = true;
+        //Disable WelcomeTrigger GameObject and Enable Level Movement
+    }
+
+    void Update()
+    {
+
+    }
+}
