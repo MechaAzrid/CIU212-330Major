7ba49d8faa28ad9814d12b25549eca67bfbf0ee0
Christopher Wrigley <1005943@student.sae.edu.au>
2018-08-07 22:47:31 +1000

    overhauled movement system

--- Assets/Greybox_Testing/Multiple_Scene_Greybox/MSG_Level_Movement.cs
+++ Assets/Greybox_Testing/Multiple_Scene_Greybox/MSG_Level_Movement.cs
@@ -19,6 +19,8 @@ public class MSG_Level_Movement : MonoBehaviour
     private Camera c;
     private Event e;
 
+    public Vector3 screen_center;
+
 
     // Use this for initialization
     void Start ()
@@ -30,12 +32,18 @@ public class MSG_Level_Movement : MonoBehaviour
 
     void OnGUI()
     {
+        screen_center = new Vector3(c.pixelWidth / 2, c.pixelHeight / 2, 0);
+
         e = Event.current;
         if (SystemInfo.deviceType == DeviceType.Desktop)
         {
             pointer_position.x = e.mousePosition.x;
             pointer_position.y = c.pixelHeight - e.mousePosition.y;
             pointer_world_position = c.ScreenToWorldPoint(new Vector3(pointer_position.x, pointer_position.y, pointer_position.z));
+
+            Vector3 differecne = pointer_position - screen_center;
+            float rotationZ = Mathf.Atan2(differecne.y, differecne.x) * Mathf.Rad2Deg;
+            view_anchor.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
         }
     }
 
@@ -52,22 +60,33 @@ public class MSG_Level_Movement : MonoBehaviour
             // Mouse Input
             if (Input.GetButton("Fire1"))
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
+                //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                //RaycastHit hit;
 
-                if (Physics.Raycast(ray, out hit, 100))
+
+                // Old Mouse Movement
+                //if (Physics.Raycast(ray, out hit, 100))
+                //{
+                //    Vector3 differecne = hit.transform.position - view_anchor.transform.position;
+                //    float rotationZ = Mathf.Atan2(differecne.y, differecne.x) * Mathf.Rad2Deg;
+                //    view_anchor.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+                //    RaycastHit blockage;
+
+                //    if (Physics.Linecast(view_anchor.transform.position, view_point.transform.position, out blockage))
+                //    {
+                //        moving = true;
+                //        player.transform.position = Vector3.MoveTowards(player.transform.position, hit.point, step);
+                //    }
+                //}
+
+                // New Mouse Movement
+                RaycastHit blockage;
+
+                if (Physics.Linecast(view_anchor.transform.position, view_point.transform.position, out blockage))
                 {
-                    Vector3 differecne = hit.transform.position - view_anchor.transform.position;
-                    float rotationZ = Mathf.Atan2(differecne.y, differecne.x) * Mathf.Rad2Deg;
-                    view_anchor.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-
-                    RaycastHit blockage;
-
-                    if (Physics.Linecast(view_anchor.transform.position, view_point.transform.position, out blockage))
-                    {
-                        moving = true;
-                        player.transform.position = Vector3.MoveTowards(player.transform.position, hit.point, step);
-                    }
+                    moving = true;
+                    player.transform.position = Vector3.MoveTowards(player.transform.position, new Vector3(view_point.transform.position.x, view_point.transform.position.y, 0), step);
                 }
             }
 
@@ -82,24 +101,38 @@ public class MSG_Level_Movement : MonoBehaviour
             if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
                 pointer_position = Input.GetTouch(0).position;
-                pointer_world_position = c.ScreenToWorldPoint(pointer_position);
 
-                Ray ray = Camera.main.ScreenPointToRay(pointer_world_position);
-                RaycastHit hit;
+                Vector3 differecne = pointer_position - screen_center;
+                float rotationZ = Mathf.Atan2(differecne.y, differecne.x) * Mathf.Rad2Deg;
+                view_anchor.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
-                if (Physics.Raycast(ray, out hit, 100))
+                //pointer_world_position = c.ScreenToWorldPoint(pointer_position);
+
+                //Ray ray = Camera.main.ScreenPointToRay(pointer_world_position);
+                //RaycastHit hit;
+
+                //if (Physics.Raycast(ray, out hit, 100))
+                //{
+                //    Vector3 differecne = hit.transform.position - view_anchor.transform.position;
+                //    float rotationZ = Mathf.Atan2(differecne.y, differecne.x) * Mathf.Rad2Deg;
+                //    view_anchor.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+                //    RaycastHit blockage;
+
+                //    if (Physics.Linecast(view_anchor.transform.position, view_point.transform.position, out blockage))
+                //    {
+                //        moving = true;
+                //        player.transform.position = Vector3.MoveTowards(player.transform.position, hit.point, step);
+                //    }
+                //}
+
+                // New Mouse Movement
+                RaycastHit blockage;
+
+                if (Physics.Linecast(view_anchor.transform.position, view_point.transform.position, out blockage))
                 {
-                    Vector3 line_check = new Vector3(hit.point.x, hit.point.y, hit.point.z - 0.1f);
-                    RaycastHit blockage;
-
-                    if (Physics.Linecast(player.transform.position, line_check, out blockage))
-                    {
-
-                    }
-                    else
-                    {
-                        player.transform.position = Vector3.MoveTowards(player.transform.position, hit.point, step);
-                    }
+                    moving = true;
+                    player.transform.position = Vector3.MoveTowards(player.transform.position, new Vector3(view_point.transform.position.x, view_point.transform.position.y, 0), step);
                 }
             }
 
@@ -107,24 +140,38 @@ public class MSG_Level_Movement : MonoBehaviour
             if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Moved)
             {
                 pointer_position = Input.GetTouch(0).position;
-                pointer_world_position = c.ScreenToWorldPoint(pointer_position);
 
-                Ray ray = Camera.main.ScreenPointToRay(pointer_world_position);
-                RaycastHit hit;
+                Vector3 differecne = pointer_position - screen_center;
+                float rotationZ = Mathf.Atan2(differecne.y, differecne.x) * Mathf.Rad2Deg;
+                view_anchor.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
-                if (Physics.Raycast(ray, out hit, 100))
+                //pointer_world_position = c.ScreenToWorldPoint(pointer_position);
+
+                //Ray ray = Camera.main.ScreenPointToRay(pointer_world_position);
+                //RaycastHit hit;
+
+                //if (Physics.Raycast(ray, out hit, 100))
+                //{
+                //    Vector3 differecne = hit.transform.position - view_anchor.transform.position;
+                //    float rotationZ = Mathf.Atan2(differecne.y, differecne.x) * Mathf.Rad2Deg;
+                //    view_anchor.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+                //    RaycastHit blockage;
+
+                //    if (Physics.Linecast(view_anchor.transform.position, view_point.transform.position, out blockage))
+                //    {
+                //        moving = true;
+                //        player.transform.position = Vector3.MoveTowards(player.transform.position, hit.point, step);
+                //    }
+                //}
+
+                // New Mouse Movement
+                RaycastHit blockage;
+
+                if (Physics.Linecast(view_anchor.transform.position, view_point.transform.position, out blockage))
                 {
-                    Vector3 line_check = new Vector3(hit.point.x, hit.point.y, hit.point.z - 0.1f);
-                    RaycastHit blockage;
-
-                    if (Physics.Linecast(player.transform.position, line_check, out blockage))
-                    {
-
-                    }
-                    else
-                    {
-                        player.transform.position = Vector3.MoveTowards(player.transform.position, hit.point, step);
-                    }
+                    moving = true;
+                    player.transform.position = Vector3.MoveTowards(player.transform.position, new Vector3(view_point.transform.position.x, view_point.transform.position.y, 0), step);
                 }
             }
 
