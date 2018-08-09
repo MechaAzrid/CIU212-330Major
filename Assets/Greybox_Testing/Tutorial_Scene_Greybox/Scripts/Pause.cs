fd874552c0c0c25877ba572b64a85f8312e6ddda
Mark <1007004@student.sae.edu.au>
2018-08-09 14:47:08 +1000

    Pause Button done and implemented

---
+++ Assets/Greybox_Testing/Tutorial_Scene_Greybox/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [Header("Pause UI")]
+    public Transform pauseCanvas;
+    public GameObject pauseButton;
+
+    public string scene;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+
+        pauseCanvas.gameObject.SetActive(true);
+        pauseButton.GetComponent<Button>().interactable = false;
+    }
+
+	public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1;
+
+        pauseCanvas.gameObject.SetActive(false);
+        pauseButton.GetComponent<Button>().interactable = true;
+    }
+}
