233d5e0e121436184312ea15c28f07bf935c51c3
Christopher Wrigley <1005943@student.sae.edu.au>
2018-08-11 13:21:44 +1000

    added resolution changer to all cameras

---
+++ Assets/Greybox_Testing/Tutorial_Scene_Greybox/Scripts/Camera_Screen_Resolution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Screen_Resolution : MonoBehaviour
+{
+    public bool adjust_screen = true;
+
+    private float defualt_width;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        defualt_width = Camera.main.orthographicSize * Camera.main.aspect;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (adjust_screen)
+        {
+            Camera.main.orthographicSize = defualt_width / Camera.main.aspect;
+        }
+	}
+}
